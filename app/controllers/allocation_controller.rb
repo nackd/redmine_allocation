7394a3a623e4f5db3e223cfc70a3f83d3fe03a32
Jose M. Prieto <jmprieto@emergya.com>
2012-05-25 15:10:20 +0200

    add a view with allocation data by user

--- app/controllers/allocation_controller.rb
+++ app/controllers/allocation_controller.rb
@@ -1,11 +1,38 @@
 class AllocationController < ApplicationController
   before_filter :find_project
   menu_item :overview
+  include Allocation::Allocation
 
   def by_project
     @projects = Project.find :all,
                              :conditions => @project.project_condition(true),
                              :include => [:members => :user],
                              :order => "#{Project.table_name}.lft"
+  end
+
+  def by_user
+    @months = months
+    user_group_field = Setting.plugin_redmine_allocation['users_custom_field']
+    project_group_field = Setting.plugin_redmine_allocation['projects_custom_field']
+    subproject_users = User.find :all,
+                                 :select => "DISTINCT #{User.table_name}.*",
+                                 :joins => { :members => :project },
+                                 :include => :members,
+                                 :conditions => @project.project_condition(true)
+    if user_group_field and project_group_field
+      group = @project.custom_value_for(project_group_field)
+      group_users = User.find :all,
+                              :joins => { :custom_values => :custom_field },
+                              :include => :members,
+                              :conditions => "#{CustomField.table_name}.id = #{user_group_field} AND #{CustomValue.table_name}.value = #{group}"
+      outside_users = subproject_users - group_users
+      group_allocation = allocation_by_months(group_users, @months)
+      outside_allocation = allocation_by_months(outside_users, @months)
+      @allocation = [[:"allocation.label_group_allocation", group_allocation],
+                     [:"allocation.label_outside_allocation", outside_allocation]]
+    else
+      all_allocation = allocation_by_months(subproject_users, @months)
+      @allocation = [[:"allocation.label_all_allocation", all_allocation]]
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 ActionController::Routing::Routes.draw do |map|
-  map.allocation_by_project 'projects/:id/allocation', :controller => 'allocation', :action => 'by_project'
+  map.allocation_by_project 'projects/:id/project_allocation', :controller => 'allocation', :action => 'by_project'
+  map.allocation_by_user 'projects/:id/user_allocation', :controller => 'allocation', :action => 'by_user'
 end
