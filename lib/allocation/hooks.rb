1a7b9a56503b74254dacf472400f05088043606b
Jose M. Prieto <jmprieto@emergya.com>
2012-06-11 17:44:29 +0200

    allways include calendar header tags in project settings
    fixes #5

--- lib/allocation/hooks.rb
+++ lib/allocation/hooks.rb
@@ -6,5 +6,7 @@ module Allocation
               :partial => 'hooks/allocation/view_projects_settings_members_table_row'
     render_on :view_projects_show_sidebar_bottom,
               :partial => 'hooks/allocation/view_projects_show_sidebar_bottom'
+    render_on :view_layouts_base_html_head,
+              :partial => 'hooks/allocation/view_layouts_base_html_head'
   end
 end
